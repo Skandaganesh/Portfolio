fc0af329c1373e7e0ee2694aed37084185808577
Skandaganesh <49191239+Skandaganesh@users.noreply.github.com>
2024-08-11 00:38:31 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          <h3 className="img-fluid logo">Portfolio </h3>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
