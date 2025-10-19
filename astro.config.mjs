186cc9fcaedca41e4c2f6ab93ace49a16a68b66d
Skanda Ganesh P V <49191239+Skandaganesh@users.noreply.github.com>
2025-10-20 03:32:16 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://skandaganesh.github.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
